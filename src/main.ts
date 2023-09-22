42eaa616dfdcac9628a1c88674275045d985a0f1
MiguelPasaje <cartel-dy@hotmail.com>
2023-09-21 22:11:27 -0500

    homework

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { setupCounter } from './counter.ts'
 
 // import './topics/01-basic.types'
 // import './topics/02-object-interface'
-import './topics/03-functions.ts'
+// import './topics/03-functions.ts'
+import './topics/04-homework-types.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   hola mundo
