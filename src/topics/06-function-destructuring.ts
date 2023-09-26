0364f2fc18219c54cc5e0742459f9e8196f94d69
MiguelPasaje <cartel-dy@hotmail.com>
2023-09-25 21:55:41 -0500

    function destructuring

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -1,42 +1,56 @@
-
-
-interface Product{
-    description: string
-    price: number
+interface Product {
+  description: string;
+  price: number;
 }
 
-const phone:Product = {
-    description:'Nokia A1',
-    price: 150.0
-}
-const tablet:Product = {
-    description:'Aipad Air',
-    price: 250.0
-} 
-interface TaxCalculationOptions{
-    tax:number
-    products: Product[]
+const phone: Product = {
+  description: "Nokia A1",
+  price: 150.0,
+};
+const tablet: Product = {
+  description: "Aipad Air",
+  price: 250.0,
+};
+interface TaxCalculationOptions {
+  tax: number;
+  products: Product[];
 }
 
-function taxCalculation(options:TaxCalculationOptions):number[]{
-    let total = 0
+function taxCalculation(options: TaxCalculationOptions): [number,number] {
+// function taxCalculation({tax,products}: TaxCalculationOptions): [number,number] {
+  let total = 0;
 
-    options.products.forEach(product => {
+  const { tax, products } = options;
+
+  /*  
+   products.forEach(product => {
         total += product.price
-    })
+    }) 
+    */
+    products.forEach(({price}) => {
+        total += price;
+    }); 
 
-    return [total, total * options.tax]
+  return [total, total * tax];
 }
 
-const shopingCart = [phone,tablet]
+const shopingCart = [phone, tablet];
 const tax = 0.15; //impuesto sobre la venta
 
 const result = taxCalculation({
-    tax,
-    products: shopingCart
-})
+  tax,
+  products: shopingCart,
+});
 
-console.log({result});
+//destructuring
+const [total, totaltax] = taxCalculation({
+  tax,
+  products: shopingCart,
+});
 
+//const {t,t2} = result
 
-export{}
+console.log({ result });
+console.log({ total, totaltax });
+
+export {};
